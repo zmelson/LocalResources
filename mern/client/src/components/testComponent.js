6a632cdba0dad82fb0387fbdac846dd9ffb75461
zmelson <zachary.p.melson@gmail.com>
2022-11-07 17:49:52 -0500

    Add basic components to frontend

---
+++ mern/client/src/components/testComponent.js
@@ -0,0 +1,13 @@
+import React from "react";
+//use below to access files in images folder
+import testImage from './images/logoPlaceholder.png';
+//use for nicer css formatting
+import "bootstrap/dist/css/bootstrap.css";
+
+export default function TestComponent() {
+ return (
+   <div>
+     <h4>(This is a stand-in component for the login process.)</h4>
+   </div>
+ );
+}
