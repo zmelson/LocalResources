26138c985415515e2c63fdacb97f8b005468dd2a
Evan Andresen <evanandresen@ufl.edu>
2022-11-08 00:45:05 -0500

    addedNavLinks

--- mern/client/src/components/navbar.js
+++ mern/client/src/components/navbar.js
@@ -26,12 +26,32 @@ export default function Navbar() {
        >
          <span className="navbar-toggler-icon"></span>
        </button>
+
+       <div className="collapse navbar-collapse" id="navbarSupportedContent">
+         <ul className="navbar-nav ml-auto">
+           <li className="nav-item">
+             <NavLink className="nav-link" to="/create">
+               Table View
+             </NavLink>
+           </li>
+         </ul>
+       </div>
+
+       <div className="collapse navbar-collapse" id="navbarSupportedContent">
+         <ul className="navbar-nav ml-auto">
+           <li className="nav-item">
+             <NavLink className="nav-link" to="/create">
+               Map View
+             </NavLink>
+           </li>
+         </ul>
+       </div>
  
        <div className="collapse navbar-collapse" id="navbarSupportedContent">
          <ul className="navbar-nav ml-auto">
            <li className="nav-item">
              <NavLink className="nav-link" to="/create">
-               Create Record
+               Contribute Resource
              </NavLink>
            </li>
          </ul>
