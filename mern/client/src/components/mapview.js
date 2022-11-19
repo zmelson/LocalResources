0607c6e9623bf55f79f903e1f4ac28e1ba8309cf
Evan Andresen <evanandresen@ufl.edu>
2022-11-19 16:52:59 -0500

    Better nav font and map link

---
+++ mern/client/src/components/mapview.js
@@ -0,0 +1,13 @@
+import React from "react";
+//use below to access files in images folder
+import testImage from './images/logoPlaceholder.png';
+//use for nicer css formatting
+import "bootstrap/dist/css/bootstrap.css";
+
+export default function MapView() {
+ return (
+   <div>
+     <h4>I'm da map</h4>
+   </div>
+ );
+}
--- mern/client/src/components/navbar.js
+++ mern/client/src/components/navbar.js
@@ -30,8 +30,8 @@ export default function Navbar() {
        <div className="collapse navbar-collapse" id="navbarSupportedContent">
          <ul className="navbar-nav ml-auto">
            <li className="nav-item">
-             <NavLink className="nav-link" to="/create">
-               Table View
+             <NavLink className="nav-link" to="/">
+               <h2>Table View</h2>
              </NavLink>
            </li>
          </ul>
@@ -40,8 +40,8 @@ export default function Navbar() {
        <div className="collapse navbar-collapse" id="navbarSupportedContent">
          <ul className="navbar-nav ml-auto">
            <li className="nav-item">
-             <NavLink className="nav-link" to="/create">
-               Map View
+             <NavLink className="nav-link" to="/map_view">
+              <h2>Map View</h2>
              </NavLink>
            </li>
          </ul>
@@ -51,7 +51,7 @@ export default function Navbar() {
          <ul className="navbar-nav ml-auto">
            <li className="nav-item">
              <NavLink className="nav-link" to="/create">
-               Contribute Resource
+               <h2>Contribute Resource</h2>
              </NavLink>
            </li>
          </ul>
@@ -61,7 +61,7 @@ export default function Navbar() {
          <ul className="navbar-nav ml-auto">
            <li className="nav-item">
              <NavLink className="nav-link" to="/login">
-               Login
+               <h2>Login</h2>
              </NavLink>
            </li>
          </ul>
