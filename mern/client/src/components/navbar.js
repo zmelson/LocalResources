3e0b20e0ff977d200a0da915b1a50be755fc1f77
zmelson <zachary.p.melson@gmail.com>
2022-11-21 14:41:30 -0500

    add google login functionality

--- mern/client/src/components/navbar.js
+++ mern/client/src/components/navbar.js
@@ -6,7 +6,25 @@ import "bootstrap/dist/css/bootstrap.css";
  
 // We import NavLink to utilize the react router.
 import { NavLink } from "react-router-dom";
- 
+
+const urlParams = new URLSearchParams(window.location.search);
+async function getClientId() {
+  const redirectUri = 'http://localhost:5000/auth/google/callback'
+  const scope = 'profile email openid'
+  const responseType = 'code'
+
+  const response = await fetch('clientId')
+  const json = await response.json()
+  document.querySelector('#link').href = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${json.clientId}&redirect_uri=${redirectUri}&scope=${scope}&response_type=${responseType}&access_type=offline&include_granted_scopes=true`
+}
+
+function loginDisplay() {
+  if (urlParams.get('id')) {
+    return `Hi, ${urlParams.get('given_name')}!`;
+  }
+  else return "Login";
+}
+
 // Here, we display our Navbar
 export default function Navbar() {
  return (
@@ -61,7 +79,7 @@ export default function Navbar() {
          <ul className="navbar-nav ml-auto">
            <li className="nav-item">
              <NavLink className="nav-link" to="/login">
-               Login
+               {loginDisplay()}
              </NavLink>
            </li>
          </ul>
--- mern/client/src/components/recordList.js
+++ mern/client/src/components/recordList.js
@@ -27,7 +27,7 @@ export default function RecordList() {
  // This method fetches the records from the database.
  useEffect(() => {
    async function getRecords() {
-     const response = await fetch(`http://localhost:3000/record/`);
+     const response = await fetch(`http://localhost:5000/record/`);
  
      if (!response.ok) {
        const message = `An error occurred: ${response.statusText}`;
@@ -46,7 +46,7 @@ export default function RecordList() {
  
  // This method will delete a record
  async function deleteRecord(id) {
-   await fetch(`http://localhost:3000/${id}`, {
+   await fetch(`http://localhost:5000/${id}`, {
      method: "DELETE"
    });
  
