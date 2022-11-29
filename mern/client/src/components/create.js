e277a1603e818854ab411b012cc38d21199ca95d
emmamastro <57884530+emmamastro@users.noreply.github.com>
2022-11-29 15:47:39 -0500

    added create,record,edit

--- mern/client/src/components/create.js
+++ mern/client/src/components/create.js
@@ -1,115 +1,111 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router";
- 
+
 export default function Create() {
- const [form, setForm] = useState({
-   Service_Provider: "",
-   Location: "",
-   Contact_Information: "",
-   Hours: "",
-   Category: "",
- });
- const navigate = useNavigate();
- 
- // These methods will update the state properties.
- function updateForm(value) {
-   return setForm((prev) => {
-     return { ...prev, ...value };
-   });
- }
- 
- // This function will handle the submission.
- async function onSubmit(e) {
-   e.preventDefault();
- 
-   // When a post request is sent to the create url, we'll add a new record to the database.
-   const newPerson = { ...form };
- 
-   await fetch("http://localhost:5000/record/add", {
-     method: "POST",
-     headers: {
-       "Content-Type": "application/json",
-     },
-     body: JSON.stringify(newPerson),
-   })
-   .catch(error => {
-     window.alert(error);
-     return;
-   });
- 
-   setForm({ Service_Provider: "",
-     Location: "",
-     Contact_Information: "",
-     Hours: "",
-     Category: "", });
-   navigate("/");
- }
- 
- // This following section will display the form that takes the input from the user.
- return (
-   <div>
-     <h3>Create New Record</h3>
-     <form onSubmit={onSubmit}>
-       <div className="form-group">
-         <label htmlFor="Service_Provider">Service_Provider</label>
-         <input
-           type="text"
-           className="form-control"
-           id="name"
-           value={form.Service_Provider}
-           onChange={(e) => updateForm({ Service_Provider: e.target.value })}
-         />
-       </div>
-       <div className="form-group">
-         <label htmlFor="Location">Location</label>
-         <input
-           type="text"
-           className="form-control"
-           id="Location"
-           value={form.Location}
-           onChange={(e) => updateForm({ Location: e.target.value })}
-         />
-       </div>
-       <div className="form-group">
-         <label htmlFor="Contact_Information">Contact_Information</label>
-         <input
-             type="text"
-             className="form-control"
-             id="Contact_Information"
-             value={form.Contact_Information}
-             onChange={(e) => updateForm({ Contact_Information: e.target.value })}
-         />
-       </div>
-       <div className="form-group">
-         <label htmlFor="Hours">Hours</label>
-         <input
-             type="text"
-             className="form-control"
-             id="Hours"
-             value={form.Hours}
-             onChange={(e) => updateForm({ Hours: e.target.value })}
-         />
-       </div>
-       <div className="form-group">
-         <label htmlFor="Category">Category</label>
-         <input
-             type="text"
-             className="form-control"
-             id="Category"
-             value={form.Category}
-             onChange={(e) => updateForm({ Category: e.target.value })}
-         />
-       </div>
+  const [form, setForm] = useState({
+    Service_Provider: "",
+    Location: "",
+    Contact_Information: "",
+    Hours: "",
+    Category: "",
+  });
+  const navigate = useNavigate();
+
+  // These methods will update the state properties.
+  function updateForm(value) {
+    return setForm((prev) => {
+      return { ...prev, ...value };
+    });
+  }
+
+  // This function will handle the submission.
+  async function onSubmit(e) {
+    e.preventDefault();
+
+    // When a post request is sent to the create url, we'll add a new record to the database.
+    const newResource = { ...form };
+
+    await fetch("http://localhost:5000/record/add", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newResource),
+    })
+        .catch(error => {
+          window.alert(error);
+          return;
+        });
+
+    setForm({ Service_Provider: "", Location: "", Contact_Information: "", Hours: "", Category: "" });
+    navigate("/");
+  }
+
+  // This following section will display the form that takes the input from the user.
+  return (
+      <div>
+        <h3>Please fill out the form below to contribute a resource to our list</h3>
+        <form onSubmit={onSubmit}>
+          <div className="form-group">
+            <label htmlFor="Service_Provider">Service Provider</label>
+            <input
+                type="text"
+                className="form-control"
+                id="Service Provider"
+                value={form.Service_Provider}
+                onChange={(e) => updateForm({ Service_Provider: e.target.value })}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="Location">Location</label>
+            <input
+                type="text"
+                className="form-control"
+                id="Location"
+                value={form.Location}
+                onChange={(e) => updateForm({ Location: e.target.value })}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="Contact_Information">Contact Information</label>
+            <input
+                type="text"
+                className="form-control"
+                id="Contact_Information"
+                value={form.Contact_Information}
+                onChange={(e) => updateForm({ Contact_Information: e.target.value })}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="Hours">Hours</label>
+            <input
+                type="text"
+                className="form-control"
+                id="Hours"
+                value={form.Hours}
+                onChange={(e) => updateForm({ Hours: e.target.value })}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="Category">Category</label>
+            <input
+                type="text"
+                className="form-control"
+                id="Category"
+                value={form.Category}
+                onChange={(e) => updateForm({ Category: e.target.value })}
+            />
+          </div>
 
 
-       <div className="form-group">
-         <input
-           type="submit"
-           value="Add Resource"
-           className="btn btn-primary"
-         />
-       </div>
-     </form>
-   </div>
- );
+          <div className="form-group">
+            <input
+                type="submit"
+                value="Add Resource"
+                className="btn btn-primary"
+            />
+          </div>
+        </form>
+      </div>
+  );
 }
--- mern/client/src/components/edit.js
+++ mern/client/src/components/edit.js
@@ -1,137 +1,137 @@
 import React, { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router";
- 
+
 export default function Edit() {
- const [form, setForm] = useState({
-   Service_Provider: "",
-   Location: "",
-   Contact_Information: "",
-   Hours: "",
-   Category: "",
- });
- const params = useParams();
- const navigate = useNavigate();
- 
- useEffect(() => {
-   async function fetchData() {
-     const id = params.id.toString();
-     const response = await fetch(`http://localhost:5000/record/${params.id.toString()}`);
- 
-     if (!response.ok) {
-       const message = `An error has occurred: ${response.statusText}`;
-       window.alert(message);
-       return;
-     }
- 
-     const record = await response.json();
-     if (!record) {
-       window.alert(`Record with id ${id} not found`);
-       navigate("/");
-       return;
-     }
- 
-     setForm(record);
-   }
- 
-   fetchData();
- 
-   return;
- }, [params.id, navigate]);
- 
- // These methods will update the state properties.
- function updateForm(value) {
-   return setForm((prev) => {
-     return { ...prev, ...value };
-   });
- }
- 
- async function onSubmit(e) {
-   e.preventDefault();
-   const editedPerson = {
-     Service_Provider: form.Service_Provider,
-     Location: form.Location,
-     Contact_Information: form.Contact_Information,
-     Hours: form.Hours,
-     Category: form.Category,
-   };
- 
-   // This will send a post request to update the data in the database.
-   await fetch(`http://localhost:5000/update/${params.id}`, {
-     method: "POST",
-     body: JSON.stringify(editedPerson),
-     headers: {
-       'Content-Type': 'application/json'
-     },
-   });
- 
-   navigate("/");
- }
- 
- // This following section will display the form that takes input from the user to update the data.
- return (
-   <div>
-     <h3>Update Record</h3>
-     <form onSubmit={onSubmit}>
-       <div className="form-group">
-         <label htmlFor="Service_Provider">Service_Provider: </label>
-         <input
-           type="text"
-           className="form-control"
-           id="Service_Provider"
-           value={form.Service_Provider}
-           onChange={(e) => updateForm({ Service_Provider: e.target.value })}
-         />
-       </div>
-       <div className="form-group">
-         <label htmlFor="Location">Location: </label>
-         <input
-           type="text"
-           className="form-control"
-           id="Location"
-           value={form.Location}
-           onChange={(e) => updateForm({ Location: e.target.value })}
-         />
-       </div>
-       <div className="form-group">
-         <label htmlFor="Contact Information">Contact_Information: </label>
-         <input
-             type="text"
-             className="form-control"
-             id="Contact_Information"
-             value={form.Contact_Information}
-             onChange={(e) => updateForm({ Contact_Information: e.target.value })}
-         />
-       </div>
-       <div className="form-group">
-         <label htmlFor="Hours">Hours: </label>
-         <input
-             type="text"
-             className="form-control"
-             id="Hours"
-             value={form.Hours}
-             onChange={(e) => updateForm({ Hours: e.target.value })}
-         />
-       </div>
-       <div className="form-group">
-         <label htmlFor="Category">Category: </label>
-         <input
-             type="text"
-             className="form-control"
-             id="Category"
-             value={form.Category}
-             onChange={(e) => updateForm({ Category: e.target.value })}
-         />
-       </div>
-       <br />
- 
-       <div className="form-group">
-         <input
-           type="submit"
-           value="Update Record"
-           className="btn btn-primary"
-         />
-       </div>
-     </form>
-   </div>
- );
+  const [form, setForm] = useState({
+    Service_Provider: "",
+    Location: "",
+    Contact_Information: "",
+    Hours: "",
+    Category: "",
+  });
+  const params = useParams();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    async function fetchData() {
+      const id = params.id.toString();
+      const response = await fetch(`http://localhost:5000/record/${params.id.toString()}`);
+
+      if (!response.ok) {
+        const message = `An error has occurred: ${response.statusText}`;
+        window.alert(message);
+        return;
+      }
+
+      const record = await response.json();
+      if (!record) {
+        window.alert(`Record with id ${id} not found`);
+        navigate("/");
+        return;
+      }
+
+      setForm(record);
+    }
+
+    fetchData();
+
+    return;
+  }, [params.id, navigate]);
+
+  // These methods will update the state properties.
+  function updateForm(value) {
+    return setForm((prev) => {
+      return { ...prev, ...value };
+    });
+  }
+
+  async function onSubmit(e) {
+    e.preventDefault();
+    const editedResource = {
+      Service_Provider: form.Service_Provider,
+      Location: form.Location,
+      Contact_Information: form.Contact_Information,
+      Hours: form.Hours,
+      Category: form.Category,
+    };
+
+    // This will send a post request to update the data in the database.
+    await fetch(`http://localhost:5000/update/${params.id}`, {
+      method: "POST",
+      body: JSON.stringify(editedResource),
+      headers: {
+        'Content-Type': 'application/json'
+      },
+    });
+
+    navigate("/");
+  }
+
+  // This following section will display the form that takes input from the user to update the data.
+  return (
+      <div>
+        <h3>Update Resource</h3>
+        <form onSubmit={onSubmit}>
+          <div className="form-group">
+            <label htmlFor="Service Provider">Service Provider: </label>
+            <input
+                type="text"
+                className="form-control"
+                id="Service Provider"
+                value={form.Service_Provider}
+                onChange={(e) => updateForm({ Service_Provider: e.target.value })}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="Location">Location: </label>
+            <input
+                type="text"
+                className="form-control"
+                id="Location"
+                value={form.Location}
+                onChange={(e) => updateForm({ Location: e.target.value })}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="Contact Information">Contact Information: </label>
+            <input
+                type="text"
+                className="form-control"
+                id="Contact_Information"
+                value={form.Contact_Information}
+                onChange={(e) => updateForm({ Contact_Information: e.target.value })}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="hours">Hours: </label>
+            <input
+                type="text"
+                className="form-control"
+                id="hours"
+                value={form.Hours}
+                onChange={(e) => updateForm({ Hours: e.target.value })}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="Category">Category: </label>
+            <input
+                type="text"
+                className="form-control"
+                id="Category"
+                value={form.Category}
+                onChange={(e) => updateForm({ Category: e.target.value })}
+            />
+          </div>
+          <br />
+
+          <div className="form-group">
+            <input
+                type="submit"
+                value="Update Resource"
+                className="btn btn-primary"
+            />
+          </div>
+        </form>
+      </div>
+  );
 }
